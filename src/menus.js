6711416952134a03ca1f64e033d7046c4f964691
Brad Buchanan <bradley.c.buchanan@gmail.com>
2017-10-30 16:15:41 -0700

    OSX-specific menu

--- src/menus.js
+++ src/menus.js
@@ -59,6 +59,24 @@ module.exports = function setupMenus() {
     }
   ];
 
+  // Special behavior for OSX to match usual menu patterns there:
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        {role: 'about'},
+        {type: 'separator'},
+        {role: 'services', submenu: []},
+        {type: 'separator'},
+        {role: 'hide'},
+        {role: 'hideothers'},
+        {role: 'unhide'},
+        {type: 'separator'},
+        {role: 'quit'}
+      ]
+    })
+  }
+
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 };
