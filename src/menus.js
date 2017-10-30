40b13c002c512c0835d5dcc24af9ae8a4c158212
Brad Buchanan <bradley.c.buchanan@gmail.com>
2017-10-30 16:32:11 -0700

    Reorganize menu logic

--- src/menus.js
+++ src/menus.js
@@ -1,82 +1,111 @@
-const {Menu, shell} = require('electron');
+const {app, Menu, shell} = require('electron');
 const packageJson = require('../package.json');
 
-module.exports = function setupMenus() {
-  const template = [
-    {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'CmdOrCtrl+R',
-          click(_, focusedWindow) {
-            if (focusedWindow) {
-              focusedWindow.webContents.send('reload-requested');
-            }
-          }
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator: (
-            process.platform === 'darwin' ?
-              'Alt+Command+I' :
-              'Ctrl+Shift+I'
-          ),
-          click(_, focusedWindow) {
-            if (focusedWindow) {
-              focusedWindow.webContents.send('toggle-dev-tools-requested');
-            }
-          }
-        },
-        {
-          label: 'Toggle Electron Developer Tools',
-          click(_, focusedWindow) {
-            if (focusedWindow) {
-              focusedWindow.webContents.toggleDevTools();
-            }
-          }
-        }
-      ]
-    },
-    {
-      role: 'help',
-      submenu: [
-        {
-          label: 'Maker Toolkit Browser',
-          enabled: false,
-        },
-        {
-          label: 'Code.org 2017',
-          enabled: false,
-        },
-        {
-          label: `Version ${packageJson.version}`,
-          click() {
-            shell.openExternal(`https://github.com/code-dot-org/browser/releases/tag/v${packageJson.version}`);
-          }
-        }
-      ]
+const RELOAD_WEBVIEW = {
+  label: 'Reload',
+  accelerator: 'CmdOrCtrl+R',
+  click(_, focusedWindow) {
+    if (focusedWindow) {
+      focusedWindow.webContents.send('reload-requested');
     }
-  ];
+  }
+};
 
-  // Special behavior for OSX to match usual menu patterns there:
+const TOGGLE_WEBVIEW_DEVTOOLS = {
+  label: 'Toggle Developer Tools',
+  accelerator: (
+    process.platform === 'darwin' ?
+      'Alt+Command+I' :
+      'Ctrl+Shift+I'
+  ),
+  click(_, focusedWindow) {
+    if (focusedWindow) {
+      focusedWindow.webContents.send('toggle-dev-tools-requested');
+    }
+  }
+};
+
+const TOGGLE_ELECTRON_DEVTOOLS = {
+  label: 'Toggle Electron Developer Tools',
+  click(_, focusedWindow) {
+    if (focusedWindow) {
+      focusedWindow.webContents.toggleDevTools();
+    }
+  }
+};
+
+const VERSION = {
+  label: `Version ${packageJson.version}`,
+  click() {
+    shell.openExternal(`https://github.com/code-dot-org/browser/releases/tag/v${packageJson.version}`);
+  }
+};
+
+module.exports = function setupMenus() {
+  // The initial menu item on Windows and Linux, often named 'File'
+  const fileMenu = {
+    label: 'File',
+    submenu: [
+      {role: 'quit'}
+    ]
+  };
+
+  // The initial menu item on OSX, always named for the application
+  const osxAppMenu = {
+    label: app.getName(),
+    submenu: [
+      {role: 'about'},
+      VERSION,
+      {type: 'separator'},
+      {role: 'services', submenu: []},
+      {type: 'separator'},
+      {role: 'hide'},
+      {role: 'hideothers'},
+      {role: 'unhide'},
+      {type: 'separator'},
+      {role: 'quit'}
+    ]
+  };
+
+  const viewMenu = {
+    label: 'View',
+    submenu: [
+      RELOAD_WEBVIEW,
+      TOGGLE_WEBVIEW_DEVTOOLS,
+      TOGGLE_ELECTRON_DEVTOOLS
+    ]
+  };
+
+  const helpMenu = {
+    label: 'Help',
+    submenu: [
+      {
+        label: 'Maker Toolkit Browser',
+        enabled: false,
+      },
+      VERSION,
+    ]
+  };
+
+  const osxHelpMenu = {
+    role: 'help'
+  };
+
+  // We define menus differently on OSX:
+  let menu;
   if (process.platform === 'darwin') {
-    template.unshift({
-      label: app.getName(),
-      submenu: [
-        {role: 'about'},
-        {type: 'separator'},
-        {role: 'services', submenu: []},
-        {type: 'separator'},
-        {role: 'hide'},
-        {role: 'hideothers'},
-        {role: 'unhide'},
-        {type: 'separator'},
-        {role: 'quit'}
-      ]
-    })
+    menu = Menu.buildFromTemplate([
+      osxAppMenu,
+      viewMenu,
+      osxHelpMenu
+    ]);
+  } else {
+    menu = Menu.buildFromTemplate([
+      fileMenu,
+      viewMenu,
+      helpMenu
+    ]);
   }
 
-  const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 };
