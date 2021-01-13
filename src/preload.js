df65e06be8473844dd7217505a7a1149fa89bf5b
Erin Peach <erin@code.org>
2021-01-12 17:28:39 -0800

    Remove Avrgirl

--- src/preload.js
+++ src/preload.js
@@ -11,7 +11,6 @@
 const SerialPort = require('serialport');
 const packageJson = require('../package.json');
 const {mayInjectNativeApi} = require('./originAllowlist');
-const Avrgirl = require('avrgirl-arduino');
 const md5 = require('blueimp-md5');
 
 function init() {
@@ -23,41 +22,7 @@ function init() {
   window.SerialPort = SerialPort;
   window.MakerBridge = {
     getVersion,
-    flashBoardFirmware,
   };
-}
-
-/**
- * Flash firmware of board with contents of specified hex file.
- * @param {Object} flashInfo, info to flash board
- * @param {String} flashInfo.boardName, name of board from Avrgirl options
- * @param {String} flashInfo.hexPath, path to hex file to flash to board
- * @param {String} flashInfo.checksum, checksum to verify valid hex download
- * @return {Promise}
- */
-function flashBoardFirmware(flashInfo) {
-  return new Promise((resolve, reject) => {
-    let avrgirl = new Avrgirl({board: flashInfo.boardName});
-    window.fetch(flashInfo.hexPath)
-      .then(function(response) {
-        // Hard coded checksum value to verify valid hex download
-        if (md5(response) === flashInfo.checksum) {
-          response.arrayBuffer().then(function(body) {
-            // Pass the response buffer to flash function to avoid filesystem error
-            avrgirl.flash(Buffer.from(body), (error) => {
-              if (error) {
-                reject(new Error(error));
-              } else {
-                console.log('Firmware Updated');
-                resolve();
-              }
-            });
-          });
-        } else {
-          reject(new Error('Error downloading firmware for board.'));
-        }
-      });
-  });
 }
 
 function getVersion() {
