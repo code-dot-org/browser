1525f16a4f923190f6010faa054d7db216ec3ed7
Aaron Waggener <aaron.waggener@code.org>
2021-05-21 14:22:17 -0700

    Notarize Mac App

---
+++ config/notarize.js
@@ -0,0 +1,18 @@
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+  console.log('notarizing app');
+
+  return await notarize({
+    appBundleId: 'com.electron.codeorg-maker-app',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+  });
+};
