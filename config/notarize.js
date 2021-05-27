117f9d4fc18b4942e848b1d3fb043791f30be2c1
Aaron Waggener <aaron.waggener@code.org>
2021-05-26 17:46:38 -0700

    Update Travis config and fix builds

--- config/notarize.js
+++ config/notarize.js
@@ -1,18 +1,24 @@
 const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;  
+  const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== 'darwin') {
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
-  console.log('notarizing app');
+  const appleId = process.env.APPLEID;
+  const appleIdPassword = process.env.APPLEIDPASS;
 
-  return await notarize({
-    appBundleId: 'com.electron.codeorg-maker-app',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-  });
+  if (appleId && appleIdPassword) {
+    console.log('notarizing Mac app. This process can take several minutes.');
+    await notarize({
+      appBundleId: 'com.electron.codeorg-maker-app',
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: appleId,
+      appleIdPassword: appleIdPassword,
+    });
+  } else {
+    console.log('APPLEID and APPLEIDPASS environment variables not set; skipped notarizing Mac app');
+  }
 };
